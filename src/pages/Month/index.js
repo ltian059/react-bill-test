63cc3dde331cfa658815bcd8f51b5cd18b673b37
litian <ltian059@uottawa.ca>
2025-04-05 19:32:22 -0400

    Enhances UI and adds monthly bill calculations
    Improves global styling with a reset for padding, margin, and box-sizing.
    Refines layout and theming with better responsiveness and localized font updates.
    Introduces logic for grouping and summarizing monthly bills, including expenditures, income, and balance calculations.
    
    Fixes inconsistencies in data formatting and improves the user experience in the monthly view.

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,9 +1,11 @@
 import { NavBar, DatePicker } from "antd-mobile";
 import { DownOutline, UpOutline } from "antd-mobile-icons";
 import "./index.scss";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import classNames from "classnames";
 import dayjs from "dayjs";
+import { useSelector } from "react-redux";
+import { groupBy, set } from "lodash";
 const Month = () => {
   //时间选择模块
   //点击打开时间选择器
@@ -12,11 +14,57 @@ const Month = () => {
   const [currDate, setCurrDate] = useState(() =>
     dayjs(new Date()).format("YYYY | M")
   );
+  //3. 计算当前月份的支出、收入和结余
+  const [currMonthBillList, setCurrMonthBillList] = useState([]);
+
+  //按月分组数据，统计月度的支出、收入和结余
+  //1. 从redux中获取数据
+  const { billList } = useSelector((state) => state.bill);
+  //2. 按月分组数据，统计月度的支出、收入和结余
+  const groupBillListByMonth = () => {
+    const map = groupBy(billList, (item) =>
+      dayjs(item.date).format("YYYY | M")
+    );
+    console.log("按月分组数据", map);
+    return map;
+  };
+
+  //2. useMemo缓存计算结果，避免重复计算
+  const billListGroupedByMonth = useMemo(
+    () => groupBillListByMonth(billList),
+    [billList]
+  );
+
+  //更改时间选择器的值
   const onConfirm = (value) => {
     setDateVisible(false);
     setCurrDate(dayjs(value).format("YYYY | M"));
     console.log("当前时间", currDate);
+    // setCurrMonthBillList(billListGroupedByMonth[currDate]);
   };
+
+  //进入页面时，默认显示当前月份的账单，并在切换月份时更新
+  useEffect(() => {
+    setCurrMonthBillList(billListGroupedByMonth[currDate]);
+  }, [billListGroupedByMonth, currDate]);
+
+  //计算当前月份总的支出、收入和结余
+  const { pay, income, balance } = useMemo(() => {
+    let pay = 0,
+      income = 0,
+      balance = 0;
+    if (currMonthBillList) {
+      currMonthBillList.forEach((item) => {
+        if (item.type === "pay") {
+          pay += item.money;
+        } else if (item.type === "income") {
+          income += item.money;
+        }
+      });
+      balance = income + pay;
+    }
+    return { pay, income, balance };
+  }, [currMonthBillList]);
 
   return (
     <div className="monthlyBill">
@@ -36,28 +84,29 @@ const Month = () => {
           {/* 统计区域 */}
           <div className="twoLineOverview">
             <div className="item">
-              <div className="money">{100}</div>
+              <div className="money">{pay.toFixed(2)}</div>
               <div className="type">支出</div>
             </div>
             <div className="item">
-              <div className="money">{100}</div>
+              <div className="money">{income.toFixed(2)}</div>
               <div className="type">收入</div>
             </div>
             <div className="item">
-              <div className="money">{100}</div>
+              <div className="money">{balance.toFixed(2)}</div>
               <div className="type">结余</div>
             </div>
           </div>
         </div>
         {/* 时间选择器 */}
         <DatePicker
+          className="datePicker"
           title="时间选择"
           precision="month"
           onClose={() => setDateVisible(false)}
           onConfirm={onConfirm}
           max={new Date()}
           visible={dateVisible}
-        />
+        ></DatePicker>
       </div>
     </div>
   );
