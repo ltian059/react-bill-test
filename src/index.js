553c404d5bf90e5d697590a2fa6e4a04912f2c0a
litian <ltian059@uottawa.ca>
2025-04-06 11:33:43 -0400

    Refactors styles and enhances layout responsiveness
    Replaces px2vw with rem for consistent scaling across devices
    Adds flexible.js for adaptive layout support
    Introduces max-width for larger screens and updates global styles
    Refines New page with improved design and added functionality
    
    Improves maintainability and ensures better cross-device compatibility

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import router from "./router";
 //注入redux store
 import { Provider } from "react-redux";
 import store from "./store";
-
+import "./flexible.js"; //引入flexible.js
 //导入定制主题文件
 import "./theme.scss";
-
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router}></RouterProvider>
+    <div className="app">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   </Provider>
 );
